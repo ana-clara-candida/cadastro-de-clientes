e872842f50795203ccd172d85aeb79f6093962b4
ana-clara-candida <88786437+ana-clara-candida@users.noreply.github.com>
2021-10-03 15:59:24 -0300

    Update Pessoa.cs

--- Pessoa.cs
+++ Pessoa.cs
@@ -1,11 +1,11 @@
 namespace CadastroCilente
 {
-    public abstract class Pessoa  //criando a classe abstrata Pessoa
+    public abstract class Pessoa  // criando a classe abstrata Pessoa
     {
-        public string nome { get; set; } // criando o atributo nome
-        public string endereco { get; set; } // criando o atributo nome
-        public bool enderecoComercial { get; set; }
-        public void pagarImposto(){
+        public string nome { get; set; } // criando o atributo de nome
+        public string endereco { get; set; } // criando o atributo de endereço
+        public bool enderecoComercial { get; set; } // criando o atributo que verifica se o endereço é comercial ou não
+        public void pagarImposto(){ // criando o método para pagar imposto
 
         }
         
