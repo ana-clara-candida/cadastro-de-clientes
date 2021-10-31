95c65e6fd362150d7053ff5a74ae949fc7263c73
Ana Clara Cândida <aclaracaldas@gmail.com>
2021-10-30 23:55:08 -0300

    comentários sobre os métodos de pagar imposto e validar CNPJ adicionados

--- PessoaJuridica.cs
+++ PessoaJuridica.cs
@@ -5,21 +5,21 @@ namespace CadastroCilente
     {
         public string cnpj { get; set; } // Criando o atributo CNPJ, que é uma string pública
         public string razaoSocial { get; set; } // Criando o atributo razão social, que também é uma string pública
-        public override double pagarImposto(double rendimento){ // Sobrescrevendo o método pagar imposto
-            if(rendimento <= 5000){
-                return rendimento * 0.06;
-        }else if(rendimento > 5000 && rendimento <= 10000){
-                return rendimento * 0.08;
-        }else{
-          return rendimento * 0.1;
+        public override double pagarImposto(double rendimento){ // Sobrescrevendo o método de pagar imposto
+            if(rendimento <= 5000){ // Se o rendimento for menor ou igual a 5000;
+                return rendimento * 0.06; // ...retorne 6% do rendimento (ou rendimento . 0,06)
+        }else if(rendimento <= 10000){ // Se o rendimento for menor ou igual a 10000...
+                return rendimento * 0.08; // ...retorne 8% do rendimento (ou rendimento . 0,08)
+        }else{ // Se não...
+          return rendimento * 0.1; //...retorne 10% do rendimento (ou rendimento . 0,1)
         }
         } 
         
-         public bool validarCnpj(string vcnpj){
-            if(vcnpj.Length == 14 && vcnpj.Substring(vcnpj.Length - 6, 4) == "0001"){
-                return true;
+         public bool validarCnpj(string vcnpj){ // Criando o método de validação de CNPJ
+            if(vcnpj.Length == 14 && vcnpj.Substring(vcnpj.Length - 6, 4) == "0001"){ // Se o tamanho da string recebida para o CNPJ for 14 e os 4 caracteres a partir da posição 6 sejam 0001...
+                return true; //...retorne verdadeiro
             }
-            return false;
+            return false; // Caso contrário, o método retorna falso
         
         
     }
